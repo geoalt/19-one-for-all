5f51b637ce42c89738f489b1207cdcaaa7ffbb9b
Diogo Martins <me.martinsfe@gmail.com>
2023-02-03 18:04:39 -0300

    req 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT
+  s.song AS nome_musica,
+  CASE 
+    WHEN s.song LIKE '%Bard%' THEN REPLACE(s.song, 'Bard', 'QA')
+    WHEN s.song LIKE '%Amar%' THEN REPLACE(s.song, 'Amar', 'Code Review')
+    WHEN s.song LIKE '%Pais%' THEN REPLACE(s.song, 'Pais', 'Pull Requests')
+    WHEN s.song LIKE '%SOUL%' THEN REPLACE(s.song, 'SOUL', 'CODE')
+    WHEN s.song LIKE '%SUPERSTAR%' THEN REPLACE(s.song, 'SUPERSTART', 'SUPERDEV')
+  END AS novo_nome
+FROM SpotifyClone.songs_table AS s
+HAVING novo_nome IS NOT NULL
+ORDER BY novo_nome DESC
