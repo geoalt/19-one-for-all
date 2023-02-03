f3027d3c4c1ce826158880a70ab56d85d0acbb42
Diogo Martins <me.martinsfe@gmail.com>
2023-02-03 15:38:33 -0300

    fix: req 4

--- desafio4.sql
+++ desafio4.sql
@@ -2,7 +2,7 @@ SELECT
   u.user AS usuario,
 IF (MAX(YEAR(p.historic_date)) < 2021, 
   'Usuário inativo',
-  'Usuário ativo') AS condicao_usuario
+  'Usuário ativo') AS status_usuario
 FROM SpotifyClone.users_table AS u
 INNER JOIN SpotifyClone.playback_history_table AS p
   ON u.id = p.user_id
